84dba6c61616f760ec78ff2f424921af73b84673
muhammad_r_fikri <muhammad_r_fikri@telkomsel.co.id>
2020-06-11 16:09:07 +0700

    repair route

--- routes/web.php
+++ routes/web.php
@@ -20,22 +20,22 @@ Route::get('login', function () {
     return view('login');
 });
 
-Route::get ('password/lost','ForgotPasswordController@forgotPassword')->middleware('auth');;
+Route::get ('password/lost','ForgotPasswordController@forgotPassword')->middleware('auth');
 
 Auth::routes();
-Route::get ('dashboard', 'DashboardController@index')->middleware('auth');;
+Route::get ('dashboard', 'DashboardController@index')->middleware('auth');
 Route::get ('changepassword', 'UserController@changepassword');
 Route::post('updatepassword','UserController@updatePassword');
-Route::get ('profile', 'UserController@profile')->middleware('auth');;
+Route::get ('profile', 'UserController@profile')->middleware('auth');
 Route::resource ('pages', 'PagesController');
 Route::post ('update/{user_id}', 'UserController@updateprofile');
 Route::post ('login', 'MainController@checklogin');
 Route::post('changePassword/{user_id}','UserController@updatePassword')->name('changePassword');
-Route::get ('user/profile', 'UserController@profile');->middleware('auth');
-Route::get ('main/logout', 'MainController@logout')->middleware('auth');;
+Route::get ('user/profile', 'UserController@profile')->middleware('auth');
+Route::get ('main/logout', 'MainController@logout')->middleware('auth');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');;
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
 
 Route::any('{catchall}', function () {
     return view('404');
